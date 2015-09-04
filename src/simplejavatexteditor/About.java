c8b8f10f5d0f5594f36dfbc1536aec05500aee14
Archie Gunasekara <achintha@me.com>
2015-09-04 13:33:53 +1000

    Comments on the source code updated

--- src/simplejavatexteditor/About.java
+++ src/simplejavatexteditor/About.java
@@ -1,14 +1,19 @@
 /**
  * @name        Simple Java NotePad
  * @package     ph.notepad
- * @file        About.java
+ * @file        UI.java
  * @author      SORIA Pierre-Henry
  * @email       pierrehs@hotmail.com
  * @link        http://github.com/pH-7
  * @copyright   Copyright Pierre-Henry SORIA, All Rights Reserved.
  * @license     Apache (http://www.apache.org/licenses/LICENSE-2.0)
  * @create      2012-05-04
- * @update      2012-05-05
+ * @update      2015-09-4
+ * 
+ * 
+ * @modifiedby  Achintha Gunasekara
+ * @modweb      http://www.achinthagunasekara.com
+ * @modemail    contact@achinthagunasekara.com
  */
 
 package simplejavatexteditor;
