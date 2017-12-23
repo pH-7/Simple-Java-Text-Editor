73c06853efa2fd6a21d74f259e3452b847c7b50b
dkovacevic <dejankov@gmail.com>
2017-12-23 13:02:07 +0100

    Minor changes

--- src/com/wire/collab/Editor.java
+++ src/com/wire/collab/Editor.java
@@ -1,19 +1,5 @@
 /**
  * This code was taken from:  https://github.com/pH-7/Simple-Java-Text-Editor
- *
- * @name Simple Java NotePad
- * @package ph.notepad
- * @file UI.java
- * @author SORIA Pierre-Henry
- * @email pierrehs@hotmail.com
- * @link http://github.com/pH-7
- * @copyright Copyright Pierre-Henry SORIA, All Rights Reserved.
- * @license Apache (http://www.apache.org/licenses/LICENSE-2.0)
- * @create 2012-05-04
- * @update 2016-21-03
- * @modifiedby Achintha Gunasekara
- * @modweb http://www.achinthagunasekara.com
- * @modemail contact@achinthagunasekara.com
  */
 
 package com.wire.collab;
@@ -34,13 +20,7 @@ import java.util.Collections;
 /**
  * This class was taken from: https://github.com/pH-7/Simple-Java-Text-Editor
  */
-
 public class Editor extends JTextPane {
-
-    final static String AUTHOR_EMAIL = "pierrehenrysoria@gmail.com";
-    final static String NAME = "PHNotePad";
-    final static String EDITOR_EMAIL = "contact@achinthagunasekara.com";
-    final static double VERSION = 3.0;
     private static final long serialVersionUID = 1L;
     private static final String CRYPTO_DIR = "data";
 
@@ -69,16 +49,12 @@ public class Editor extends JTextPane {
         Logger.info(String.format("Logged in as: %s, id: %s", email, userId));
 
         WireClient wireClient = repo.getWireClient(userId, conv);
-        Conversation conversation = wireClient.getConversation();
 
-        String sharedId = conversation.members.get(0).id;
+        String title = constructTitle(userId, wireClient);
 
-        User user = new ArrayList<>(wireClient.getUsers(Collections.singletonList(userId))).get(0);
-        User sharing = new ArrayList<>(wireClient.getUsers(Collections.singletonList(sharedId))).get(0);
+        MyFrame ui = new MyFrame(title);
 
         DocumentListener listener = new MyDocumentListener(wireClient);
-
-        MyFrame ui = new MyFrame(String.format("%s sharing with %s", user.name, sharing.name));
 
         Document document = ui.getDocument();
         document.addDocumentListener(listener);
@@ -87,5 +63,21 @@ public class Editor extends JTextPane {
         handler.setListener(listener);
 
         ui.setVisible(true);
+    }
+
+    private static String constructTitle(String userId, WireClient wireClient) {
+        try {
+            Conversation conversation = wireClient.getConversation();
+
+            String sharedId = conversation.members.get(0).id;
+
+            User user = new ArrayList<>(wireClient.getUsers(Collections.singletonList(userId))).get(0);
+            User sharing = new ArrayList<>(wireClient.getUsers(Collections.singletonList(sharedId))).get(0);
+
+            return String.format("%s sharing with %s", user.name, sharing.name);
+        } catch (Exception e) {
+            Logger.warning(e.getMessage());
+            return "Proteus";
+        }
     }
 }
